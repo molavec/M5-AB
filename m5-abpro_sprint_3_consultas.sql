436606870a3b5209a50ae7a8655e1f92a8d96031
Miguel Olave <molavec@gmail.com>
2023-03-01 21:44:50 -0300

    ajuste consulta

--- m5-abpro_sprint_3_consultas.sql
+++ m5-abpro_sprint_3_consultas.sql
@@ -2,6 +2,23 @@
 
 
 /* consulta 1 */
+show create table cliente;
+show create table capacitacion;
+show create table asistentes;
+
+select * from cliente;
+select * from capacitacion;
+select * from asistentes;
+
+SELECT 
+  asist.asistnombrecompleto AS Nombre_Completo,
+  cli.cliedad AS Edad,
+  asist.asistcorreo AS Correo_Electronico
+FROM cliente cli
+JOIN capacitacion cap ON cli.rutcliente = cap.cliente_rutcliente
+JOIN asistentes asist ON cap.idcapacitacion = asist.capacitacion_idcapacitacion;
+
+
 
 /* consulta 2 */
 
